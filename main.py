e1e087a28008e4e876966ade32c6b018a34f99b6
gokulsodar <gokulssodar@gmail.com>
2025-10-25 18:40:45 +0300

    changed IP

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT")) # for render deployment
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
